85ef321b7ad499b0a5b2b0a1537e322ecbb662f7
Oleksii Onyshchenko <oleksii.onyshchenko@synapse.com>
2018-12-05 12:54:18 +0200

    Creating clear project

---
+++ src/main/java/com/oleksii/arrmy/CrewPortal/CrewPortalApplication.java
@@ -0,0 +1,12 @@
+package com.oleksii.arrmy.CrewPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrewPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrewPortalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/oleksii/arrmy/CrewPortal/CrewPortalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.oleksii.arrmy.CrewPortal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrewPortalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
